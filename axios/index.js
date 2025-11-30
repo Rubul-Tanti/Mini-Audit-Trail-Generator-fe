7635c387f82e9d47576ddbdb077273ab2a328e9d
Rubul-Tanti <rubultanti404@gmail.com>
2025-12-01 01:05:26 +0530

    added live backend link

--- axios/index.js
+++ axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl='http://localhost:8000'
+const baseUrl='https://mini-audit-trail-generator-be.onrender.com'
 const api=axios.create({
     baseURL:baseUrl
 })
